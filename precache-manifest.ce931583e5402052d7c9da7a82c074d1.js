2bd33c2b4f558392445190a12ae08b572fea26de
AtanasRaykovPapucharov <atanas.papucharov@gmail.com>
2019-12-16 16:26:28 +0200

    update

---
+++ precache-manifest.ce931583e5402052d7c9da7a82c074d1.js
@@ -0,0 +1,438 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51671391d80fbac78ee47c8cf897d4e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "670959c435a1a8246d15",
+    "url": "/static/css/main.07df1a23.chunk.css"
+  },
+  {
+    "revision": "5b226f6307fac3c1d544",
+    "url": "/static/js/2.3b387a27.chunk.js"
+  },
+  {
+    "revision": "3145aee7106485f9ebea2c8944617e54",
+    "url": "/static/js/2.3b387a27.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0045b8ce56f070acd198",
+    "url": "/static/js/3.d3431ac4.chunk.js"
+  },
+  {
+    "revision": "670959c435a1a8246d15",
+    "url": "/static/js/main.acdde219.chunk.js"
+  },
+  {
+    "revision": "12fa22f33e1f5d5906f4",
+    "url": "/static/js/runtime-main.e4caa337.js"
+  },
+  {
+    "revision": "3aca071882c3cc963d0313f6f0187a19",
+    "url": "/static/media/10.3aca0718.png"
+  },
+  {
+    "revision": "6f37d30f26f7feb18056f22ab05ad99d",
+    "url": "/static/media/12.6f37d30f.png"
+  },
+  {
+    "revision": "f74f4dca42e24918ae6f8df0e91c94dc",
+    "url": "/static/media/450-f.f74f4dca.png"
+  },
+  {
+    "revision": "d61f1f87fd74161eb339f5c89ae09fa9",
+    "url": "/static/media/450.d61f1f87.png"
+  },
+  {
+    "revision": "4719e99056737cd2fa4119160bea55af",
+    "url": "/static/media/5.4719e990.jpg"
+  },
+  {
+    "revision": "c89fc45e5f04cd143ded81bf1e61f339",
+    "url": "/static/media/6.c89fc45e.jpg"
+  },
+  {
+    "revision": "321a6a8013e9c199224599228d4571e2",
+    "url": "/static/media/Genuinely-bitter-12.321a6a80.png"
+  },
+  {
+    "revision": "e27ce00b24d3fdae42fc2a30a881010c",
+    "url": "/static/media/Klasik.e27ce00b.png"
+  },
+  {
+    "revision": "6d2aae9fc160cffe45df6f422e9b7432",
+    "url": "/static/media/Palivec.6d2aae9f.png"
+  },
+  {
+    "revision": "0a33b9a82daf09a027d1a921f977d239",
+    "url": "/static/media/Palivec42.0a33b9a8.png"
+  },
+  {
+    "revision": "c79710ca9c72eddc0216c8035ae4a514",
+    "url": "/static/media/Razna-10.c79710ca.png"
+  },
+  {
+    "revision": "d3109b49f518b17f8d91f1e9d119b4f2",
+    "url": "/static/media/Svijany.d3109b49.png"
+  },
+  {
+    "revision": "f77b380e8344c3445979768026f51513",
+    "url": "/static/media/SvijanyMaz.f77b380e.mp4"
+  },
+  {
+    "revision": "456a0fd589726f6430f16044ca58e0dd",
+    "url": "/static/media/ale-vs-lager.456a0fd5.jpg"
+  },
+  {
+    "revision": "80cff0d043b914a234c1afe59b6b3d07",
+    "url": "/static/media/ales-vs-lagers-2.80cff0d0.png"
+  },
+  {
+    "revision": "96a9a176f5a164790e78d6d54a55bb75",
+    "url": "/static/media/bakalar-beers.96a9a176.jpg"
+  },
+  {
+    "revision": "ee6d64e07ba8b1d22d6a6594bfbfdb13",
+    "url": "/static/media/bakalar-logo.ee6d64e0.png"
+  },
+  {
+    "revision": "367bf1dbcf4c116bfde73423615f39e8",
+    "url": "/static/media/baron.367bf1db.png"
+  },
+  {
+    "revision": "856778b37ccf78d263197a07a833a494",
+    "url": "/static/media/barrel-aging.856778b3.jpg"
+  },
+  {
+    "revision": "0ae20417c40bf352b4d225731be833d7",
+    "url": "/static/media/beer-bouquet.0ae20417.jpg"
+  },
+  {
+    "revision": "5c0b234fbabe9c0270ebdef2a58203fb",
+    "url": "/static/media/beer-ingredients.5c0b234f.jpg"
+  },
+  {
+    "revision": "97ae669000078f5a7732d8443f2ca2f4",
+    "url": "/static/media/beer.97ae6690.png"
+  },
+  {
+    "revision": "b5c456f3b0877d3b7664ab4fb976ffd5",
+    "url": "/static/media/bg-flag.b5c456f3.png"
+  },
+  {
+    "revision": "399725d196b03f51973b4c80dbdda485",
+    "url": "/static/media/bg-home.399725d1.jpg"
+  },
+  {
+    "revision": "e4f9163aadb79d5f74d16f0fd30afe35",
+    "url": "/static/media/bouquet.e4f9163a.jpg"
+  },
+  {
+    "revision": "cfc346f28a444524e951774fb1e57aa0",
+    "url": "/static/media/bronze1.cfc346f2.png"
+  },
+  {
+    "revision": "28215cc46413dc0c28c4c549beb65ee6",
+    "url": "/static/media/budweiser-girl.28215cc4.jpg"
+  },
+  {
+    "revision": "1f047d37c5edcdcfa8dfcc9b7c72a505",
+    "url": "/static/media/budweiser.1f047d37.png"
+  },
+  {
+    "revision": "a4ea89b68ffd17829d755b1d2133e0f9",
+    "url": "/static/media/celia-logo.a4ea89b6.png"
+  },
+  {
+    "revision": "23ecace41b5352bb6c3885f00ddbe980",
+    "url": "/static/media/celia.23ecace4.jpg"
+  },
+  {
+    "revision": "e7536c2a0aa3a184d4086c7e0a56e85c",
+    "url": "/static/media/celia.e7536c2a.png"
+  },
+  {
+    "revision": "d2a4c9ea69160c143ae5e937b7d160e8",
+    "url": "/static/media/cernovar.d2a4c9ea.png"
+  },
+  {
+    "revision": "1a4d90672c99138fb02a59d2afb0ad3e",
+    "url": "/static/media/dark.1a4d9067.webp"
+  },
+  {
+    "revision": "426603ad698a17a129e586b5d27b6d76",
+    "url": "/static/media/dark.426603ad.png"
+  },
+  {
+    "revision": "53b1d9fc070252750be3bb37d0e6d6f1",
+    "url": "/static/media/dark.53b1d9fc.png"
+  },
+  {
+    "revision": "f82ab9e43e8afb66f83ae5f37eeb5534",
+    "url": "/static/media/dark.f82ab9e4.png"
+  },
+  {
+    "revision": "5d7493efdbc22eddbec368eea0e85f87",
+    "url": "/static/media/desitka-f.5d7493ef.png"
+  },
+  {
+    "revision": "3562f00ea8dc2b832e9e48acc3d97d55",
+    "url": "/static/media/desitka.3562f00e.png"
+  },
+  {
+    "revision": "4ad4c4385ed139500b9cca9346b844b7",
+    "url": "/static/media/desitka.4ad4c438.png"
+  },
+  {
+    "revision": "9217faafd8189e7c3efe3fcd4a0b7096",
+    "url": "/static/media/drak.9217faaf.png"
+  },
+  {
+    "revision": "a009156053877e2bf562b32b6f09da3d",
+    "url": "/static/media/drink.a0091560.jpg"
+  },
+  {
+    "revision": "fc740fc9789cd731b3a334cb4b24976c",
+    "url": "/static/media/dudak_svanda.fc740fc9.png"
+  },
+  {
+    "revision": "77b8838f4ec44fa38f4054f27c631868",
+    "url": "/static/media/dvanactka-tmava.77b8838f.png"
+  },
+  {
+    "revision": "0cb0a4f66fc43d53361a49fa1a5ba47c",
+    "url": "/static/media/exportni.0cb0a4f6.png"
+  },
+  {
+    "revision": "d87f3776a66dde7ee1516338875a4faa",
+    "url": "/static/media/gb.d87f3776.svg"
+  },
+  {
+    "revision": "0d76f930590b78e1a778b49bcf07623c",
+    "url": "/static/media/girl3.0d76f930.jpg"
+  },
+  {
+    "revision": "880891a6f3febd99a69249b7d83c94ab",
+    "url": "/static/media/granat.880891a6.png"
+  },
+  {
+    "revision": "f877b9af5a6d95730680a124290e9b65",
+    "url": "/static/media/granat.f877b9af.png"
+  },
+  {
+    "revision": "b4cda7ed0faa2204dc2540b8697e0d1e",
+    "url": "/static/media/green.b4cda7ed.webp"
+  },
+  {
+    "revision": "b05cb9889837d82e79cfc22e8d018205",
+    "url": "/static/media/jadenacka.b05cb988.png"
+  },
+  {
+    "revision": "752eb72c3ea95e8b27e7651ec8a786ec",
+    "url": "/static/media/keg-beer-tap.752eb72c.jpg"
+  },
+  {
+    "revision": "3004913ccd992b59acd17dcd231ff784",
+    "url": "/static/media/key-ale-and-lager-styles.3004913c.png"
+  },
+  {
+    "revision": "66f64a3f3976c787b7a00afb19f30a94",
+    "url": "/static/media/knezna.66f64a3f.webp"
+  },
+  {
+    "revision": "5d85c345396b614b4d4a424a711d6456",
+    "url": "/static/media/kozel-11.5d85c345.png"
+  },
+  {
+    "revision": "bbc3b5761899abb7f52a1a423e2d47b3",
+    "url": "/static/media/kozel-cerny.bbc3b576.png"
+  },
+  {
+    "revision": "8d33eb384e8cdd8d2e26307b9173ae20",
+    "url": "/static/media/kozel-pale.8d33eb38.png"
+  },
+  {
+    "revision": "47e516b4913d4d0d0b12205de9ca4a17",
+    "url": "/static/media/kozel.47e516b4.jpg"
+  },
+  {
+    "revision": "40ffbfc5b194049fbe535fd890fa70e2",
+    "url": "/static/media/lager.40ffbfc5.jpg"
+  },
+  {
+    "revision": "e138fd4600de60f735a9328cb884d622",
+    "url": "/static/media/lezak.e138fd46.png"
+  },
+  {
+    "revision": "470dc259a9c708ac88ad13c964c8bd58",
+    "url": "/static/media/logo-cerna-hora.470dc259.png"
+  },
+  {
+    "revision": "c866b4f2c9adda2212bd615a1552362c",
+    "url": "/static/media/logo-dudak.c866b4f2.png"
+  },
+  {
+    "revision": "05737332b42a58c4a4aa0badc3bd79ab",
+    "url": "/static/media/logo-gambrinus-znacky.05737332.png"
+  },
+  {
+    "revision": "dde3ccf2ad167e4b24cd7aa01544c253",
+    "url": "/static/media/logo-klasik.dde3ccf2.png"
+  },
+  {
+    "revision": "889d26bf1e1d0a375bc6eb6f37d113fe",
+    "url": "/static/media/logo-kozel-znacky.889d26bf.png"
+  },
+  {
+    "revision": "b7b375d38d9b7315ee9ea8dd5e4da221",
+    "url": "/static/media/logo-krusovice.b7b375d3.png"
+  },
+  {
+    "revision": "cb746d27b9693057c2f381487b4d2572",
+    "url": "/static/media/logo-pilsner-urquell-znacky.cb746d27.png"
+  },
+  {
+    "revision": "e63e87d79cdf9fc48fec2178bcb6d4a3",
+    "url": "/static/media/logo-radegast-znacky.e63e87d7.png"
+  },
+  {
+    "revision": "4f9f5402b4925c61af873839ec0b12b7",
+    "url": "/static/media/logo-samson.4f9f5402.png"
+  },
+  {
+    "revision": "031e9f462bf5387b668ed928e86340d0",
+    "url": "/static/media/logo.031e9f46.png"
+  },
+  {
+    "revision": "06d2d91ea318e922dbc7d7bc8deaec94",
+    "url": "/static/media/logo.06d2d91e.png"
+  },
+  {
+    "revision": "5144503ab15784ebceb81997b76c1807",
+    "url": "/static/media/logo.5144503a.png"
+  },
+  {
+    "revision": "521a922a7899418a25550fd51e426110",
+    "url": "/static/media/logo.521a922a.png"
+  },
+  {
+    "revision": "70772e036855b6791a8e4362a18366b3",
+    "url": "/static/media/logo.70772e03.png"
+  },
+  {
+    "revision": "94fc6117581e9795061357252c144f73",
+    "url": "/static/media/logo.94fc6117.png"
+  },
+  {
+    "revision": "bb92413d88751d0620d1f4507e70abbe",
+    "url": "/static/media/logo.bb92413d.png"
+  },
+  {
+    "revision": "4d28a85ee3a524690b03829052e50d62",
+    "url": "/static/media/maz-f.4d28a85e.png"
+  },
+  {
+    "revision": "a5e32cf5755a9290c03fefdbff214242",
+    "url": "/static/media/maz.a5e32cf5.png"
+  },
+  {
+    "revision": "caad77153a9ec8309d4dd633a60fb124",
+    "url": "/static/media/medium.caad7715.png"
+  },
+  {
+    "revision": "21a7b7509a416ac064152e67abfaeb96",
+    "url": "/static/media/medovy.21a7b750.png"
+  },
+  {
+    "revision": "fdc0e5c40952a70715b76a4726a0791a",
+    "url": "/static/media/mistruv.fdc0e5c4.webp"
+  },
+  {
+    "revision": "435bf3c5cfa0be3eff8460c11949b309",
+    "url": "/static/media/original-10.435bf3c5.png"
+  },
+  {
+    "revision": "c2f0dde3f6f8259de15ad970beac388a",
+    "url": "/static/media/pale-12.c2f0dde3.png"
+  },
+  {
+    "revision": "5224e1c3f0e0278c11ba6dbb19b869d3",
+    "url": "/static/media/pilsner.5224e1c3.png"
+  },
+  {
+    "revision": "45e834f769e35a44d69fa72f461d7e9c",
+    "url": "/static/media/psenicna.45e834f7.png"
+  },
+  {
+    "revision": "9ff8ea40c49db13fd14bfe112698dd77",
+    "url": "/static/media/rezana-desitka.9ff8ea40.webp"
+  },
+  {
+    "revision": "8f1bdba0bb3add101dd6dd2ec9ca095b",
+    "url": "/static/media/rezany.8f1bdba0.png"
+  },
+  {
+    "revision": "ada7b8050554a0a2b5040132a1697682",
+    "url": "/static/media/rytir-f.ada7b805.png"
+  },
+  {
+    "revision": "58918af34b5c91d5fed15127c7bb6ef5",
+    "url": "/static/media/rytir.58918af3.png"
+  },
+  {
+    "revision": "8e095f92d38ed6b30bf288284a876539",
+    "url": "/static/media/shop1.8e095f92.jpg"
+  },
+  {
+    "revision": "4189c2b738ae97c4262ebf3e74c21721",
+    "url": "/static/media/smichkov.4189c2b7.png"
+  },
+  {
+    "revision": "1c792b40192b172d4d7c16a0ca02dcc3",
+    "url": "/static/media/stare-brno-svetle.1c792b40.png"
+  },
+  {
+    "revision": "c6a54bfdddc2c0a41c727e8d3a23b7b8",
+    "url": "/static/media/staropramen-logo.c6a54bfd.png"
+  },
+  {
+    "revision": "0639f47f0b199ae84f4a317591010b39",
+    "url": "/static/media/svetle.0639f47f.webp"
+  },
+  {
+    "revision": "821e1b90dc33f6fa94d3a714133a474d",
+    "url": "/static/media/svijany-gallery.821e1b90.png"
+  },
+  {
+    "revision": "8ace798f0f416bbe1dae00b11d991877",
+    "url": "/static/media/svijany-girl.8ace798f.jpg"
+  },
+  {
+    "revision": "d870f4f9aed12a394695b2bca2fa1e21",
+    "url": "/static/media/svijany-girl2.d870f4f9.jpg"
+  },
+  {
+    "revision": "398b70dc09a388c9ef57a39ffa404c62",
+    "url": "/static/media/tas.398b70dc.png"
+  },
+  {
+    "revision": "b10cf08c32f33faab8814aa15f2e00f6",
+    "url": "/static/media/unfiltr.b10cf08c.png"
+  },
+  {
+    "revision": "11ed0b58023fc9e1a426c5fdeb84b477",
+    "url": "/static/media/velen.11ed0b58.webp"
+  },
+  {
+    "revision": "c9eed6a9c92a15594f029e5605797a0a",
+    "url": "/static/media/vozka.c9eed6a9.png"
+  },
+  {
+    "revision": "34bda7fb7d3c93b3d57b41b425d82715",
+    "url": "/static/media/vyrocny.34bda7fb.png"
+  },
+  {
+    "revision": "2f2e812d534ca63d7bb7466398e1c5d3",
+    "url": "/static/media/za-studena-chmeleny.2f2e812d.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5517a0054d6fc90a141b620935fd52a8.js"
+  "/precache-manifest.ce931583e5402052d7c9da7a82c074d1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
